05950fbde93630cdb519f47073d3a1991d5d579f
Motaz Hesham <motazhesham01@gmail.com>
2024-09-07 00:57:40 +0300

    make centeral domain in env

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,7 +16,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [ 
-        'local.magico',
+        env('CENTERAL_DOMAIN'),
     ],
 
     /**
